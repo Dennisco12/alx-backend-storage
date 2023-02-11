58cbf85f39d6c5b38928eae604c0f8fc60853b49
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-02-11 02:25:21 -0800

    Added tasks 5 to 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,5 @@
+-- This creates a view that lists all students that have a score under 80
+-- and no last_meeting of more than 1 month
+
+CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
